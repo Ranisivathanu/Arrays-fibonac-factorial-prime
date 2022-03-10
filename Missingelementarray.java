19003f10ae6d80e285ea335c988956e89d65e41a
Ranisivathanu <100157311+Ranisivathanu@users.noreply.github.com>
2022-03-10 13:35:01 +0530

    Add files via upload

---
+++ Missingelementarray.java
@@ -0,0 +1,31 @@
+package week3.day1;
+
+
+import java.util.Arrays;
+
+public class Missingelementarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] number = {6, 3, 2, 4, 1};
+	        Arrays.sort(number);
+	        System.out.println("Missing Number is:" + getMissingNumber(number));
+	    }
+
+	    private static Integer getMissingNumber(int[] number) {
+	        Integer missed = null;
+	        for (int i = 0; i < number.length; i++) {
+	            int index = i + 1;
+	            if (number[i] != index) {
+	                missed = index;
+	                break;
+	            }
+	        }
+	        System.out.println(missed);
+			return missed;
+	    }
+	}
+	
+	
+
+
